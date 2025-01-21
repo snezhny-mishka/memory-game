eb99bdb1d536c4a46bb0505f5ef199422b363c85
unknown <xopox2015@gmail.com>
2025-01-21 16:01:15 +0100

    card back images added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const cards = document.querySelectorAll(".card");
 
+//setting the back image for each card
+cards.forEach(
+    (el, index) => {
+       let img = document.createElement("img");
+       img.src = "/memory-game/images/back-pattern.jpg";
+       el.appendChild(img); 
+    }
+);
