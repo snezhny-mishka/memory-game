cc8e8ebe749c1ad341e30e782a830d4d32eb0362
unknown <xopox2015@gmail.com>
2025-01-30 19:28:08 +0100

    Flip card

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const cards = document.querySelectorAll(".card");
 
 //setting the back image for each card
-cards.forEach(
-    (el, index) => {
-       let img = document.createElement("img");
-       img.src = "/memory-game/images/back-pattern.jpg";
-       el.appendChild(img); 
-    }
-);
+// cards.forEach(
+//     (el, index) => {
+//        let img = document.createElement("img");
+//        img.src = "/memory-game/images/back-pattern.jpg";
+//        el.appendChild(img); 
+//     }
+// );
+
+[...cards].forEach(card => {
+    card.addEventListener("click", () => {
+        card.classList.toggle("flip-card");
+        setTimeout(() => {
+            card.classList.toggle("flip-card")
+        }, 2500);
+    })
+});
+
